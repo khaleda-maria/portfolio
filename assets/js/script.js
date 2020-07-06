64dd6c27e500cfdbaea867b959641fcad7515f8e
Maria <maria.khaleda.mk@gmail.com>
2020-07-06 15:11:38 +0900

    First Commit

---
+++ assets/js/script.js
@@ -0,0 +1,134 @@
+
+/* =========================================
+                Progress Bars
+============================================ */
+$(function () {
+
+    $("#progress-elements").waypoint(function () {
+
+        $(".progress-bar").each(function () {
+
+            $(this).animate({
+                width: $(this).attr("aria-valuenow") + "%"
+            }, 2000);
+
+        });
+
+        this.destroy();
+    }, {
+        offset: 'bottom-in-view'
+    });
+
+});
+
+/* =========================================
+               Portfolio
+============================================ */
+$(window).on('load', function () {
+
+    // Initialize Isotope
+    $("#isotope-container").isotope({});
+
+    // filter items on button click
+    $("#isotope-filters").on('click', 'button', function () {
+
+        // get filter value
+        var filterValue = $(this).attr('data-filter');
+
+        // filter portfolio
+        $("#isotope-container").isotope({
+            filter: filterValue
+        });
+
+        // active button
+        $("#isotope-filters").find('.active').removeClass('active');
+        $(this).addClass('active');
+    });
+});
+
+
+/* =========================================
+              Navigation
+============================================ */
+
+/* Show & Hide White Navigation */
+$(function () {
+
+    // show/hide nav on page load
+    showHideNav();
+
+    $(window).scroll(function () {
+
+        // show/hide nav on window's scroll
+        showHideNav();
+    });
+
+    function showHideNav() {
+
+        if ($(window).scrollTop() > 50) {
+
+            // Show white nav
+            $("nav").addClass("white-nav-top");
+
+            // Show dark logo
+            // $(".navbar-brand img").attr("src", "img/logo/logo-dark.png");
+
+            // Show back to top button
+            $("#back-to-top").fadeIn();
+
+        } else {
+
+            // Hide white nav
+            $("nav").removeClass("white-nav-top");
+
+            // Show logo
+            // $(".navbar-brand img").attr("src", "img/logo/logo.png");
+
+            // Hide back to top button
+            $("#back-to-top").fadeOut();
+        }
+    }
+});
+
+// Smooth Scrolling
+$(function () {
+
+    $("a.smooth-scroll").click(function (event) {
+
+        event.preventDefault();
+
+        // get section id like #about, #servcies, #work, #team and etc.
+        var section_id = $(this).attr("href");
+
+        $("html, body").animate({
+            scrollTop: $(section_id).offset().top -64
+        }, 1250);
+
+    });
+
+});
+
+/* =========================================
+                Animation
+============================================ */
+// animate on scroll
+$(function () {
+    new WOW().init();
+});
+
+/* =========================================
+              Mobile Menu
+============================================ */
+$(function () {
+
+    // Show mobile nav
+    $("#mobile-nav-open-btn").click(function () {
+        $("#mobile-nav").css("height", "100%");
+    });
+
+    // Hide mobile nav
+    $("#mobile-nav-close-btn, #mobile-nav a").click(function () {
+        $("#mobile-nav").css("height", "0%");
+    });
+
+});
